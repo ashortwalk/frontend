e4652db85b02964e6babcd9a33139d4bc79c3064
dayounglee <verdantjuly@kakao.com>
2024-12-04 12:00:35 +0900

    [Feat] css

--- src/component/landingPage/Section03.jsx
+++ src/component/landingPage/Section03.jsx
@@ -3,7 +3,11 @@ import "./Section03.css";
 export default function Section03() {
   return (
     <div className="Section03">
-      <img src="section03.png" alt="section03.png" />
+      <img
+        className="section03-image"
+        src="section03.png"
+        alt="section03.png"
+      />
     </div>
   );
 }
