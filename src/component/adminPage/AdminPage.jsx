b16d586cecc4ca9980f984098084610995686328
dayounglee <verdantjuly@kakao.com>
2024-12-06 10:29:55 +0900

    [Fix] admin page

--- src/component/adminPage/AdminPage.jsx
+++ src/component/adminPage/AdminPage.jsx
@@ -12,19 +12,12 @@ export default function AdminPage() {
   const [groupName, setGroupName] = useState("");
   const authorization = sessionStorage.getItem("Authorization");
   const [currentPath, setCurrentPath] = useState("");
-  const [isAdmin, setIsAdmin] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(false); // 기본값을 false로 설정
+
   useEffect(() => {
     setCurrentPath(window.location.pathname);
-    async function fetchReports() {
-      const response = await axios.get(
-        `https://ashortwalk.store/api/reports?page=${currentPage}`,
-        {
-          headers: { Authorization: authorization },
-        }
-      );
 
-      setReports(response.data);
-    }
+    // 관리자 여부 체크
     async function fetchTotalPages() {
       const response = await axios.get(
         "https://ashortwalk.store/api/reports/count",
@@ -32,45 +25,56 @@ export default function AdminPage() {
           headers: { Authorization: authorization },
         }
       );
-      if (response.stauts == 200 || response.status == 201) {
+
+      // 서버 응답이 200, 201일 때만 관리자로 설정
+      if (response.status === 200 || response.status === 201) {
         setIsAdmin(true);
+        setTotalPages(Math.ceil(response.data / 3)); // 총 페이지 수 계산
       } else {
         setIsAdmin(false);
       }
-      setTotalPages(Math.ceil(response.data / 3));
+    }
+
+    async function fetchReports() {
+      const response = await axios.get(
+        `https://ashortwalk.store/api/reports?page=${currentPage}`,
+        {
+          headers: { Authorization: authorization },
+        }
+      );
+      setReports(response.data);
     }
 
     fetchTotalPages();
     fetchReports();
   }, [currentPage]);
 
+  // 관리자 여부 확인 후 리다이렉트
   useEffect(() => {
-    const checkRedirect = () => {
-      if (!isAdmin) {
-        window.location.href = "https://ashortwalk.store";
-      }
-    };
-    checkRedirect();
-  }, [isAdmin]);
+    if (!isAdmin) {
+      window.location.href = "https://ashortwalk.store";
+    }
+  }, [isAdmin]); // isAdmin 값이 변경될 때마다 실행
 
+  // 신고 처리 함수
   async function deleteContent(reportId) {
     const response = await axios.delete(
       `https://ashortwalk.store/api/reports/${reportId}`,
-      {
-        headers: { Authorization: authorization },
-      }
+      { headers: { Authorization: authorization } }
     );
-    if (200 <= response.status < 300) {
+    if (response.status >= 200 && response.status < 300) {
       alert("신고가 처리되어 콘텐츠가 삭제되었습니다.");
       window.location.reload();
     }
   }
+
+  // 그룹 삭제 함수
   async function deleteGroup() {
     const response = await axios.delete(
       `https://ashortwalk.store/api/groups/${groupName}`,
       { headers: { Authorization: authorization } }
     );
-    if (300 > response.status >= 200) {
+    if (response.status >= 200 && response.status < 300) {
       alert("그룹이 삭제되었습니다.");
     }
   }
@@ -78,84 +82,82 @@ export default function AdminPage() {
   return (
     <div>
       <Header />
-      {isAdmin && (
-        <div className="group-outer-box">
-          <h1 className="admin-title">Admin</h1>
-          <div className="group-inner-box">
-            <div className="group-box">
-              <div className="group-border-box">
-                {/* 그룹삭제 */}
-                <div className="admin-box">
-                  <h2 className="admin-subtitle">그룹 삭제</h2>
-                  <div className="group-name-box">
-                    <label className="admin-label">그룹 이름</label>
-                    <input
-                      className="group-name"
-                      onChange={(e) => {
-                        e.preventDefault();
-                        setGroupName(e.target.value);
-                      }}
-                    />
-                  </div>
-                  <button
-                    className="group-delete-button"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      deleteGroup();
-                    }}
-                  >
-                    그룹 삭제
-                  </button>
-                </div>
-              </div>
-            </div>
-
+      <div className="group-outer-box">
+        <h1 className="admin-title">Admin</h1>
+        <div className="group-inner-box">
+          <div className="group-box">
             <div className="group-border-box">
-              <h2 className="report-subtitle">신고 내역</h2>
-              <div id="report-list">
-                {reports.map((report) => {
-                  return (
-                    <div
-                      className={`report-content-box ${
-                        reports.length === 0 ? "empty" : ""
-                      }`}
-                    >
-                      <h3>제목 : {report.reportTitle}</h3>
-                      <p>내용 : {report.reportContent}</p>
-                      <div className="report-button-box">
-                        <button
-                          onClick={(e) => {
-                            e.preventDefault();
-                            window.location.href = `/${report.contentType}/${report.contentId}`;
-                          }}
-                        >
-                          조회
-                        </button>
-                        <button
-                          onClick={(e) => {
-                            e.preventDefault();
-                            deleteContent(report.id);
-                          }}
-                        >
-                          처리
-                        </button>
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-              <div className="report-pagination">
-                <Pagination
-                  currentPage={currentPage}
-                  setCurrentPage={setCurrentPage}
-                  totalPages={totalPages}
-                ></Pagination>
+              {/* 그룹삭제 */}
+              <div className="admin-box">
+                <h2 className="admin-subtitle">그룹 삭제</h2>
+                <div className="group-name-box">
+                  <label className="admin-label">그룹 이름</label>
+                  <input
+                    className="group-name"
+                    onChange={(e) => {
+                      e.preventDefault();
+                      setGroupName(e.target.value);
+                    }}
+                  />
+                </div>
+                <button
+                  className="group-delete-button"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    deleteGroup();
+                  }}
+                >
+                  그룹 삭제
+                </button>
               </div>
             </div>
           </div>
-        </div>
-      )}
 
+          <div className="group-border-box">
+            <h2 className="report-subtitle">신고 내역</h2>
+            <div id="report-list">
+              {reports.map((report) => {
+                return (
+                  <div
+                    className={`report-content-box ${
+                      reports.length === 0 ? "empty" : ""
+                    }`}
+                    key={report.id}
+                  >
+                    <h3>제목 : {report.reportTitle}</h3>
+                    <p>내용 : {report.reportContent}</p>
+                    <div className="report-button-box">
+                      <button
+                        onClick={(e) => {
+                          e.preventDefault();
+                          window.location.href = `/${report.contentType}/${report.contentId}`;
+                        }}
+                      >
+                        조회
+                      </button>
+                      <button
+                        onClick={(e) => {
+                          e.preventDefault();
+                          deleteContent(report.id);
+                        }}
+                      >
+                        처리
+                      </button>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="report-pagination">
+              <Pagination
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                totalPages={totalPages}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
